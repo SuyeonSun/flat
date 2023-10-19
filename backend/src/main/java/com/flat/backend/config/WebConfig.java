bd82b9aee10d22dad5d56591b8fda34dd2f4df8a
SuyeonSun <suninisy@gmail.com>
2023-10-19 17:38:50 +0900

    cors config 설정

--- backend/src/main/java/com/flat/backend/config/WebConfig.java
+++ backend/src/main/java/com/flat/backend/config/WebConfig.java
@@ -1,2 +1,18 @@
-package com.flat.backend.config;public class WebConfig {
+package com.flat.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3000);
+    }
 }
